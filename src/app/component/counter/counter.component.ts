052cd4632aa9aa5f370cb5495449fef5cc673a13
w91379137 <w913791379137@gmail.com>
2022-08-15 21:46:12 +0800

    [feat] 增加 單向綁定 跟 (click)

--- src/app/component/counter/counter.component.ts
+++ src/app/component/counter/counter.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
+  title: string = 'abc'
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
   constructor() { }
 
   ngOnInit(): void {
+    // this.title = 2;
   }
 
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+  onClicked(str: string): void {
+    console.log('gg', str);
+  }
+
+  isLargeBy5(num: number): boolean {
+    return num > 5;
+  }
 }
