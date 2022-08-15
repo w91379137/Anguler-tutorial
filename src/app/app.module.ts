3f55d08f01cf28eb717920d89daa4ef80a260892
w91379137 <w913791379137@gmail.com>
2022-08-15 20:33:20 +0800

    [feat] 新增 home counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { CounterComponent } from './component/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
